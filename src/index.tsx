6e2c6d1b903a55e62d2c7e38b315efa40f8ecd7c
rmkeezer <rmkeezer@yahoo.com>
2016-11-08 11:55:50 -0500

    Added electron remove ts and react

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { Nav } from "./components/Nav";
-
-ReactDOM.render(
-    <Nav />,
-    document.getElementById("example")
-);
